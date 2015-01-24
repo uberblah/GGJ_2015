e422078e5cb7630da5e135c6b2f1231ad17342dc
uberblah <miles.rufatlatre@colorado.edu>
2015-01-23 22:05:56 -0700

    added Player class

---
+++ Assets/Objects/Actor/Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Actor {
+    public Camera view = null;
+
+    protected override Vector2 getMove()
+    {
+        return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    protected override Vector2 getTarget()
+    {
+        Vector2 mpos = Input.mousePosition;
+        Vector3 mpos3 = view.ScreenToWorldPoint(new Vector3(mpos.x, mpos.y, 0.0f));
+        return new Vector2(mpos3.x, mpos3.y);
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        if (view == null)
+        {
+            view = GetComponent<Camera>();
+            if (view == null) Debug.Log(name + " failed to find its camera");
+        }
+    }
+}
