5ae05c6143fe5c83443496084af30700eae1167a
uberblah <miles.rufatlatre@colorado.edu>
2015-01-24 09:49:32 -0700

    override Start

--- Assets/Objects/Tools/Stick.cs
+++ Assets/Objects/Tools/Stick.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Stick : Tool
 {
 public float stickLength; // How long is the stick?
-public void Start ()
+public override void Start ()
 {
     toolName = "Stick";
     toolDesc = "This is your basic stick, it whacks things";
