006db342c6c20382b2b8a3c796fd77b29669750b
CdeSnowFrost <brittanylynn404@gmail.com>
2015-01-24 00:25:14 -0700

    Untabbed :(

--- Assets/Objects/Tools/Stick.cs
+++ Assets/Objects/Tools/Stick.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 public class Stick : Tool
 {
-public float stickLength; // How long is the stick?
+    public float stickLength; // How long is the stick?
 void Start ()
 {
-toolName = "Stick";
-toolDesc = "This is your basic stick, it whacks things";
+    toolName = "Stick";
+    toolDesc = "This is your basic stick, it whacks things";
 }
 
 override void Activate ()
 {
 base.Activate ();
-		Vector2 vec = this.body.GetVector;
-		float roatation = this.body.rotation;
-		body.AddForceAtPosition (new Vector2 (vec.x+(Math.Sin(rotatation)*stickLength), (Math.Cos(rotation)*stickLength)*vec.y));
+    Vector2 vec = this.body.GetVector;
+    float roatation = this.body.rotation;
+    body.AddForceAtPosition (new Vector2 (vec.x+(Math.Sin(rotatation)*stickLength), (Math.Cos(rotation)*stickLength)*vec.y));
 }
 }
 
