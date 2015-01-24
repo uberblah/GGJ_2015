cbddc96a11466566ffd8d3f28b62bb2c4be713c3
CdeSnowFrost <brittanylynn404@gmail.com>
2015-01-24 00:04:15 -0700

    NOW THEY'RE HERE, still messing with stick, because I'm super rusty with coding

---
+++ Assets/Objects/Tools/Stick.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class Stick : Tool
+{
+
+void Start ()
+{
+toolName = "Stick";
+toolDesc = "This is your basic stick, it whacks things";
+}
+
+override void Activate ()
+{
+base.Activate ();
+	
+}
+}
+
