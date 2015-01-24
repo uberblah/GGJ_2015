355804e066d2d693fc6b9313d7b3c2d4797b8367
CdeSnowFrost <brittanylynn404@gmail.com>
2015-01-23 22:46:30 -0700

    Tabs, not spaces, sorry :(

--- Assets/Objects/Tool.cs
+++ Assets/Objects/Tool.cs
@@ -6,37 +6,37 @@ using UnityEngine;
  * It can only be used if it's owned. If it isn't owned, it's just an object.
  */
 
-	public class Tool
-	{
-	String toolName; //What's the tool called?
-	String toolDesc; //Short description of the tool
+public class Tool
+{
+    String toolName; //What's the tool called?
+    String toolDesc; //Short description of the tool
 
-	Rigidbody2D body; //The tool has to have a body!
+    Rigidbody2D body; //The tool has to have a body!
 	
-	#region Owner Stuff
-	Actor owner; // Who owns the tool?
+    #region Owner Stuff
+    Actor owner; // Who owns the tool?
 
-	// Does this tool have an owner?
-	bool isOwned()
-	{
-		return owner != null;
-	}
-	// Give this tool an owner
-	virtual void giveOwner(Actor myNewOwner)
-	{
-		owner = myNewOwner;
-	}
-	// Make this tool lack an owner
-	virtual void loseOwner(Actor ownerWhoDoesntWantMe)
-	{
-		owner = null;
-	}
-	#endregion
+    // Does this tool have an owner?
+    bool isOwned()
+    {
+    return owner != null;
+    }
+    // Give this tool an owner
+    virtual void giveOwner(Actor myNewOwner)
+    {
+    owner = myNewOwner;
+    }
+    // Make this tool lack an owner
+    virtual void loseOwner(Actor ownerWhoDoesntWantMe)
+    {
+    owner = null;
+    }
+    #endregion
 
-	virtual void Activate()
-	{
-		//This is the activate function, its use differs on the tool. A stick pokes, a torch burns.
-	}
+    virtual void Activate()
+    {
+    //This is the activate function, its use differs on the tool. A stick pokes, a torch burns.
+    }
 
-	}
+}
 
