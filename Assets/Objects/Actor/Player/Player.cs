527caf2072211282558d2e38bc4c735e9bdb34eb
uberblah <miles.rufatlatre@colorado.edu>
2015-01-24 08:48:06 -0700

    metafiles for init art, actor tool and context interactions

--- Assets/Objects/Actor/Player/Player.cs
+++ Assets/Objects/Actor/Player/Player.cs
@@ -19,6 +19,20 @@ public class Player : Actor {
         return new Vector2(mpos3.x, mpos3.y);
     }
 
+    protected override int GetAction()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (Input.GetAxisRaw("Action" + i) > 0.0f) return i;
+        }
+        return 0;
+    }
+
+    protected override bool GetUseTool()
+    {
+        return Input.GetAxisRaw("Tool") > 0.0f;
+    }
+
     protected override void Start()
     {
         base.Start();
@@ -31,7 +45,7 @@ public class Player : Actor {
         forceMul = force;
     }
 
-    void Update()
+    protected override void Update()
     {
         base.Update();
 
