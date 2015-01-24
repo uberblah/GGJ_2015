7ab9201fb507f1046a56c360ffc5eba0f9bd8eb8
CdeSnowFrost <brittanylynn404@gmail.com>
2015-01-24 12:31:30 -0700

    Untabbed :(

--- Assets/Objects/LandChunk.cs
+++ Assets/Objects/LandChunk.cs
@@ -2,26 +2,26 @@ using System;
 using UnityEngine;
 public class LandChunk
 {
-	public float Height;
-	public enum landTypes {water, notWater};
-	public landTypes landType;
-	public LandChunk()
-	{
-		Height = 0.0f;
-		landType = landTypes.water;
-	}
+    public float Height;
+    public enum landTypes {water, notWater};
+    public landTypes landType;
+    public LandChunk()
+    {
+    Height = 0.0f;
+    landType = landTypes.water;
+    }
 
-	public void DetermineForm()
-	{
-		if (Height >= .25) 
-		{
-			landType = landTypes.notWater;
-		} 
-		else 
-		{
-			landType = landTypes.water;
-		}
-	}
+public void DetermineForm()
+    {
+    if (Height >= .25) 
+    {
+    landType = landTypes.notWater;
+    } 
+    else 
+    {
+    landType = landTypes.water;
+    }
+    }
 
 }
 
