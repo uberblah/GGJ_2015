1828014cb3edb6a9b7654dd0f46c45f0909247ba
william mcpherson <william.d.mcpherson@gmail.com>
2015-01-23 22:08:35 -0700

    Hover GUI now working

--- Assets/Objects/ContextObject.cs
+++ Assets/Objects/ContextObject.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ContextObject : MonoBehaviour {
+public class ContextObject : MonoBehaviour
+{
+    public Camera cam;
+    protected bool      showMenu;
 
 	// Use this for initialization
 	void Start ()
     {
-	
+        showMenu = false;
 	}
 
+    // Show menu
+    void OnGUI()
+    {
+        if (showMenu)
+        {
+            //Camera camera = GameObject.Find("Main Camera");
+            Vector2 guiPos = cam.WorldToScreenPoint (this.transform.position);
+            GUI.BeginGroup(new Rect(guiPos.x, guiPos.y, 100, 150));
+            GUI.Box(new Rect(0,0,100,150),"1. Test\n2.Test\n3.Test");
+            GUI.EndGroup();
+        }
+    }
+
+    // Show menu on mouse over
     void OnMouseOver()
     {
-        Debug.Log("Mouse Over");
+        showMenu = true;
+    }
+
+    void OnMouseExit()
+    {
+        showMenu = false;
     }
 	
 	// Update is called once per frame
