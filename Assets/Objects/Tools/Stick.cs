9d41b8ebb31ebb1e60236a7f96526be9e65067e9
CdeSnowFrost <brittanylynn404@gmail.com>
2015-01-24 00:09:38 -0700

    Works nowish

--- Assets/Objects/Tools/Stick.cs
+++ Assets/Objects/Tools/Stick.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Stick : Tool
 {
-
+public float stickLength; // How long is the stick?
 void Start ()
 {
 toolName = "Stick";
@@ -13,7 +13,7 @@ toolDesc = "This is your basic stick, it whacks things";
 override void Activate ()
 {
 base.Activate ();
-	
+		body.AddForceAtPosition (new Vector2 (this.body.GetVector * this.body.transform.rotation * stickLength));
 }
 }
 
