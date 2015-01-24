52bcfd675ec42f8d59d45070814bd70cecf942ab
william mcpherson <william.d.mcpherson@gmail.com>
2015-01-24 12:03:37 -0700

    Enemy now moves towards/away in 4sec intervals

--- Assets/Objects/Actor/Enemy/enemy.cs
+++ Assets/Objects/Actor/Enemy/enemy.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class enemy : Actor
 {
-    protected enum EnemyState { Idle, Chase };
+    protected enum EnemyState { Idle, Chase, Retreat };
 
     protected GameObject      player; // Reference to player
     protected EnemyState      state; // Current AI state
     protected float           lastSwitch; // Last time we switched states
+    protected Vector2         moveVec; // Direction we want to move in
 
 	// Use this for initialization
     protected override void Start()
@@ -20,21 +21,13 @@ public class enemy : Actor
         // Set starting state
         state = EnemyState.Idle;
         lastSwitch = Time.time;
+        moveVec = Vector2.zero;
 	}
 
     protected override Vector2 GetMove()
     {
-        // Decide how to move based on what state we're in
-        switch (state)
-        {
-            case EnemyState.Idle:
-                return Vector2.zero;
-            case EnemyState.Chase:
-                // Move towards player
-                return player.transform.position - transform.position;
-            default:
-                return Vector2.zero;
-        }
+        // Send movement direction
+        return moveVec;
     }
 	
 	// Update is called once per frame
@@ -51,21 +44,47 @@ public class enemy : Actor
             case EnemyState.Chase:
                 DoChase();
                 break;
+            case EnemyState.Retreat:
+                DoRetreat();
+                break;
             default:
                 DoIdle();
                 break;
         }
 	}
 
+    // Change to another state
+    // Recommend usage as it also sets switch time
+    protected void SwitchState(EnemyState newState)
+    {
+        state = newState;
+        lastSwitch = Time.time;
+    }
+
     protected virtual void DoIdle()
     {
         // Change to chase after amount of time
         if (Time.time > lastSwitch + 4.0f)
-            state = EnemyState.Chase;
+            SwitchState(EnemyState.Chase);
+
+        moveVec = Vector2.zero;
     }
 
     protected virtual void DoChase()
     {
+        // Change to retreat after amount of time
+        if(Time.time > lastSwitch + 4.0f)
+            SwitchState(EnemyState.Retreat);
+        // Move us towards player
+        moveVec = player.transform.position - transform.position;
+    }
 
+    protected virtual void DoRetreat()
+    {
+        // Change to chase after amount of time
+        if (Time.time > lastSwitch + 4.0f)
+            SwitchState(EnemyState.Chase);
+        // Move away from player
+        moveVec = transform.position - player.transform.position;
     }
 }
