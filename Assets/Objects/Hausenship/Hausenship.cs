6e7116f067fc54835be5290b99a384482df9471c
uberblah <miles.rufatlatre@colorado.edu>
2015-01-25 04:34:47 -0700

    asteroidsgit add .

--- Assets/Objects/Hausenship/Hausenship.cs
+++ Assets/Objects/Hausenship/Hausenship.cs
@@ -10,23 +10,36 @@ public class Hausenship : MonoBehaviour
     public float floor = -10.0f;
     public float ceil = 10.0f;
     public string crashScene = "test.unity";
+    bool gui = false;
+    public float cutDelay = 2.0f;
+    float cut = 0.0f;
 
     void Update()
     {
+        if (gui && Time.time - cut >= cutDelay) Application.LoadLevel(crashScene);
         //should tell camera to follow
         cam.transform.position = new Vector3(transform.position.x, 0.0f, 0.0f) + camOffset;
         //should respond to user input by climbing
         if (Input.anyKeyDown)
             rigidbody.velocity = new Vector3(rigidbody.velocity.x, climb, 0.0f);
-        //should point in its direction of travel (TODO)
-        
         //should cut to crash scene if it falls too low
-        if (transform.position.y < floor) Application.LoadLevel(crashScene);
+        if (transform.position.y < floor) Cut();
         climb -= decay * Time.deltaTime;
     }
 
     void OnCollisionEnter(Collision c)
     {
-        Application.LoadLevel(crashScene);
+        Cut();
+    }
+
+    void OnGUI()
+    {
+        if(gui) GUI.Box(new Rect(10, 10, 600, 50), "CRASH!!!\n(crash landing in progress)");
+    }
+
+    void Cut()
+    {
+        gui = true;
+        cut = Time.time;
     }
 }
