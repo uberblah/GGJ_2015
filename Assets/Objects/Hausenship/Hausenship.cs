1fec209ba155e1779b12158111f5230803abd65d
uberblah <miles.rufatlatre@colorado.edu>
2015-01-25 00:03:03 -0700

    hausenship doom mechanic

--- Assets/Objects/Hausenship/Hausenship.cs
+++ Assets/Objects/Hausenship/Hausenship.cs
@@ -5,21 +5,27 @@ public class Hausenship : MonoBehaviour
 {
     public Camera cam;
     public Vector3 camOffset = new Vector3(0.0f, 0.0f, -5.0f);
-    public float climb = 1.0f;
+    public float climb = 10.0f;
+    public float decay = -0.1f;
     public float floor = -10.0f;
     public string crashScene = "crash";
 
     void Update()
     {
         //should tell camera to follow
-        cam.transform.position = transform.position + camOffset;
+        cam.transform.position = new Vector3(transform.position.x, 0.0f, 0.0f) + camOffset;
         //should respond to user input by climbing
-        if (Input.anyKeyDown) rigidbody.velocity = new Vector3(rigidbody.velocity.x, climb, 0.0f);
+        if (Input.anyKeyDown)
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, climb, 0.0f);
         //should point in its direction of travel
         
         //should cut to crash scene if it falls too low
         if (transform.position.y < floor) Application.LoadLevel(crashScene);
-        //should cut to crash scene if it hits anything
+        climb -= decay * Time.deltaTime;
+    }
 
+    void OnCollisionEnter(Collision c)
+    {
+        Application.LoadLevel(crashScene);
     }
 }
