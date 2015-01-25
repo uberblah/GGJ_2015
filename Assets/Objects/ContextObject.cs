abf5be6ee6949f80e39e38ca0f068b8ff4ee450d
william mcpherson <william.d.mcpherson@gmail.com>
2015-01-25 09:35:50 -0700

    Test scene polish, commented out contextobject
    ContextObject commented out to remove errors

--- Assets/Objects/ContextObject.cs
+++ Assets/Objects/ContextObject.cs
@@ -12,66 +12,66 @@ public class ContextObject : MonoBehaviour
 
     public Camera                   cam;
     public Player                   player;
-    protected static ContextObject  curr;
-    protected bool                  showMenu;
+    //protected static ContextObject  curr;
+    //protected bool                  showMenu;
 
     public List<KeyValuePair<string, ContextMethod>> methods;
 
 	// Use this for initialization
 	void Start ()
     {
-        showMenu = false;
-        methods = new List<KeyValuePair<string, ContextMethod>>();
-        //TEMPORARY
-        methods.Add(new KeyValuePair<string,ContextMethod>("Test 1", DoTest));
-        methods.Add(new KeyValuePair<string,ContextMethod>("Test 2", DoTest));
-        methods.Add(new KeyValuePair<string,ContextMethod>("Test 3", DoTest));
+        //showMenu = false;
+        //methods = new List<KeyValuePair<string, ContextMethod>>();
+        ////TEMPORARY
+        //methods.Add(new KeyValuePair<string,ContextMethod>("Test 1", DoTest));
+        //methods.Add(new KeyValuePair<string,ContextMethod>("Test 2", DoTest));
+        //methods.Add(new KeyValuePair<string,ContextMethod>("Test 3", DoTest));
 	}
 
     // Show menu
     void OnGUI()
     {
-        if (showMenu)
-        {
-            Vector2 guiPos = cam.WorldToScreenPoint(this.transform.position);
-            GUI.BeginGroup(new Rect(guiPos.x, guiPos.y, 100, 150));
-            // Build list 
-            string list = "";
-            int i = 1;
-            foreach (KeyValuePair<string, ContextMethod> m in methods)
-            {
-                list += i + ". " + m.Key + "\n";
-                i++;
-            }
-            // Show box
-            GUI.Box(new Rect(0,0,100,150),list);
-            GUI.EndGroup();
-        }
+        //if (showMenu)
+        //{
+        //    Vector2 guiPos = cam.WorldToScreenPoint(this.transform.position);
+        //    GUI.BeginGroup(new Rect(guiPos.x, guiPos.y, 100, 150));
+        //    // Build list 
+        //    string list = "";
+        //    int i = 1;
+        //    foreach (KeyValuePair<string, ContextMethod> m in methods)
+        //    {
+        //        list += i + ". " + m.Key + "\n";
+        //        i++;
+        //    }
+        //    // Show box
+        //    GUI.Box(new Rect(0,0,100,150),list);
+        //    GUI.EndGroup();
+        //}
     }
 
     // Show menu on mouse over
     private void OnMouseOver()
     {
-        showMenu = true;
-        curr = this;
-        player.selected = this;
+        //showMenu = true;
+        //curr = this;
+        //player.selected = this;
     }
 
     private void OnMouseExit()
     {
-        showMenu = false;
-        if (curr == this)
-            curr = null;
-        if(player.selected == this) player.selected = null;
+        //showMenu = false;
+        //if (curr == this)
+        //    curr = null;
+        //if(player.selected == this) player.selected = null;
     }
 
     public void DoMethod(Actor a, int method)
     {
-        if (method <= methods.Count) methods[method - 1].Value(a, method - 1);
+        //if (method <= methods.Count) methods[method - 1].Value(a, method - 1);
     }
 
     public void DoTest(Actor a, int idx)
     {
-        Debug.Log(a.name + " called #" + idx + " in " + name);
+        //Debug.Log(a.name + " called #" + idx + " in " + name);
     }
 }
--- Assets/Objects/Tools/LaserGun.cs
+++ Assets/Objects/Tools/LaserGun.cs
@@ -8,7 +8,7 @@ public class LaserGun : Tool
     public override void Start()
     {
         base.Start();
-        showMenu = false; // Temporary
+        //showMenu = false; // Temporary
     }
 
     public override void Activate()
