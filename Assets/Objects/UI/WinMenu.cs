68dacce797ae55751cc43ea5c783e68ae5632c41
uberblah <miles.rufatlatre@colorado.edu>
2015-01-25 11:54:22 -0700

    added buttons for win menu

---
+++ Assets/Objects/UI/WinMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinMenu : MonoBehaviour
+{
+    public string initLevel = "hillbilly";
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(initLevel);
+    }
+}
