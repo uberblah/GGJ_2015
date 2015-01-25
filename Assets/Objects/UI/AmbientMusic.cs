c15a6427080b5f9b2be5a81a87fa6134c187a6a3
william mcpherson <william.d.mcpherson@gmail.com>
2015-01-25 07:40:18 -0700

    Main scene now plays music

---
+++ Assets/Objects/UI/AmbientMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientMusic : MonoBehaviour
+{
+    public AudioClip[] musics;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    // Choose a song to play
+        AudioSource aSrc = GetComponent<AudioSource>();
+        aSrc.clip = musics[(int)Random.Range(0, musics.Length - 1)];
+        aSrc.Play();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
