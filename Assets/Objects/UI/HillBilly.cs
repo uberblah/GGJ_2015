4f4eb64d20f49a05991a20603c08ead378e4c30a
uberblah <miles.rufatlatre@colorado.edu>
2015-01-25 06:51:25 -0700

    Hillbilly menu

---
+++ Assets/Objects/UI/HillBilly.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HillBilly : MonoBehaviour {
+    public string introScene = "Scenes/intro.unity";
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        Application.LoadLevel(introScene);
+    }
+}
